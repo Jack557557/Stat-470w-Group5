4fdee822f37df11efdfbd77210e8e60616a42a0a
James Weaver <jbw5332@psu.edu>
2020-02-20 11:12:09 -0500

    Added Comments for Letter Boxplots

--- SesameStreetProject.R
+++ SesameStreetProject.R
@@ -1,1 +1,20 @@
 SesData = read.csv("SesameStreetData.csv", header = T)
+SesData
+attach(SesData)
+
+####Create Boxplot
+
+##Boxplot for letters vs. view Category
+boxplot(Difflet~viewcat,
+        data=SesData,
+        main="Different boxplots for each View Category",
+        xlab="View Category",
+        ylab="Difference in Post-Pre Test for Letters",
+        col="orange",
+        border="brown"
+)
+
+##Boxplot for letters vs. sex
+##Boxplot for letters vs. setting
+##Boxplot for letters vs. viewenc
+##Boxplot for letters vs. agecat
