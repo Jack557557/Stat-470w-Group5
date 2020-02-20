0c14436784184ac87cc9c013c6f753cc0b857bc1
James Weaver <jbw5332@psu.edu>
2020-02-20 11:24:57 -0500

    Added numbers for boxplots

--- SesameStreetProject.R
+++ SesameStreetProject.R
@@ -2,12 +2,12 @@ SesData = read.csv("SesameStreetData.csv", header = T)
 SesData
 attach(SesData)
 
-####Create Boxplot
+####Create Boxplots for EDA
 
 ##Boxplot for letters vs. view Category
 boxplot(Difflet~viewcat,
         data=SesData,
-        main="Different boxplots for each View Category",
+        main="Different boxplots for each View Category vs. Letters",
         xlab="View Category",
         ylab="Difference in Post-Pre Test for Letters",
         col="orange",
@@ -15,9 +15,44 @@ boxplot(Difflet~viewcat,
 )
 
 ##Boxplot for letters vs. sex
+boxplot(Difflet~sex,
+        data=SesData,
+        main="Different boxplots for each Sex vs. Letters",
+        xlab="Sex",
+        ylab="Difference in Post-Pre Test for Letters",
+        col="orange",
+        border="brown"
+)
+
 ##Boxplot for letters vs. setting
+boxplot(Difflet~setting,
+        data=SesData,
+        main="Different boxplots for each Setting vs. Letters",
+        xlab="Setting",
+        ylab="Difference in Post-Pre Test for Letters",
+        col="orange",
+        border="brown"
+)
+
 ##Boxplot for letters vs. viewenc
+boxplot(Difflet~viewenc,
+        data=SesData,
+        main="Different boxplots for each Viewing Encouragement Level vs. Letters",
+        xlab="Viewing Encouragement Level",
+        ylab="Difference in Post-Pre Test for Letters",
+        col="orange",
+        border="brown"
+)
+
 ##Boxplot for letters vs. agecat
+boxplot(Difflet~agecat,
+        data=SesData,
+        main="Different boxplots for each Age Category vs. Letters",
+        xlab="Age Category",
+        ylab="Difference in Post-Pre Test for Letters",
+        col="orange",
+        border="brown"
+)
 
 ##Boxplot for numbers vs. viewcat
 ##Boxplot for numbers vs. sex
